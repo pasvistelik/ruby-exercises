40516e4d5eeea3ff1983c72ff2144e3cddb178df
Andrey Pasvistelik <pasvistelik@gmail.com>
2018-02-11 10:38:43 +0300

    Update 21.rb

--- 21.rb
+++ 21.rb
@@ -1,7 +1,27 @@
-def get_proper_divisors(n)
+require 'benchmark'
+
+def get_proper_divisors2(n)
   tmp = (2..Math.sqrt(n).ceil).select{|num| n%num==0}
   tmp += tmp.reverse.map{|num| n/num}
   tmp.unshift(1)
 end
 
-p (2..10_000).select{|num| num==get_proper_divisors(get_proper_divisors(num).reduce{|sum,i| sum+i}).reduce{|sum,i| sum+i}}.reduce{|sum,i| sum+i}
+def get_proper_divisors(n)
+  (2..Math.sqrt(n).ceil)
+    .select{|num| n%num==0}
+    .flat_map{|num| [num, n/num]}
+    .unshift(1)
+end
+
+def is_amicable?(num)
+  num == get_proper_divisors(get_proper_divisors(num).reduce(:+)).reduce(:+)
+end
+
+#(2..10).each{|n| p get_proper_divisors(n)}
+
+puts Benchmark.measure {
+  p (2..10_000).select{|num| is_amicable?(num)}.reduce(:+)
+}
+puts Benchmark.measure {
+  p (2..10_000).inject(0){|sum, num| is_amicable?(num) ? sum+num : sum}
+}
